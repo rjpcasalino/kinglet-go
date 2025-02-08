47b9a2c35016110e1c0c647110461e2eb149bbfe
rjpc <ryan@rjpc.net>
2025-02-07 20:46:21 -0800

    web server stuff

---
+++ ch7/http/http4.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/text/currency"
+)
+
+func main() {
+	db := database{"shoes": "50", "socks": "5"}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/currency", db.usd)
+	log.Fatal(http.ListenAndServe("[::1]:8000", nil))
+}
+
+type database map[string]string // dollars in book
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db database) usd(w http.ResponseWriter, req *http.Request) {
+	t1799, _ := time.Parse("2006-01-02", "1799-01-01")
+	for it := currency.Query(currency.Date(t1799)); it.Next(); {
+		from := ""
+		if t, ok := it.From(); ok {
+			from = t.Format("2006-01-02")
+		}
+		fmt.Fprintf(w, "%v is used in %v since: %v\n", it.Unit(), it.Region(), from)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module kinglet-go
 
 go 1.23.5
 
-require golang.org/x/net v0.34.0
+require (
+	golang.org/x/net v0.34.0
+	golang.org/x/text v0.21.0
+)
